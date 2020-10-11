cf347cffd8edd2fc33fd86d56bc8b17c08b571de
zaime ali <zaimeali1997@gmail.com>
2020-10-11 12:49:06 +0500

    Lecture 18 Done

---
+++ app/Models/BlogPost.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BlogPost extends Model
+{
+    use HasFactory;
+
+    // because we haven't used naming convention correctly
+    // we have to define the table name
+
+    // protected $table = 'blogposts';
+
+    // or we can do
+    // php artisan make:migration change_blogposts_table_name --table=blogposts
+}

dca1d40009377c78e419ed48fb53aa088006caaa
zaime ali <zaimeali1997@gmail.com>
2020-10-22 00:57:21 +0500

    Lecture 41 Done

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -4,6 +4,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\BlogPost;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -12,10 +13,28 @@ class PostTest extends TestCase
     use RefreshDatabase;
     // it helps in recreating dB structure by running all the migration on each single test run
 
-    public function testExample()
+    public function testNoBlogPostsWhenNothingInDatabase()
     {
-        $response = $this->get('/');
+        $response = $this->get('/posts');
+        $response->assertSeeText('No Posts!');
+    }
 
-        $response->assertStatus(200);
+    public function testSee1BlogPostWhenThereIs1()
+    {
+        // Arrange
+        $post = new BlogPost();
+        $post->title = "Test Case Title";
+        $post->content = "Test Case Content";
+        $post->save();
+
+        // Act
+        $response = $this->get('/posts');
+
+        // Assert
+        $response->assertSeeText('Test Case Title');
+        $this->assertDatabaseHas('blog_posts', [
+            // 'title' => 'Test Title',
+            'title' => 'Test Case Title',
+        ]);
     }
 }
